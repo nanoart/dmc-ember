5e393f730a1abc66951db9c0ed930e75166baf54
Mingfa Ma <mingfa.ma'deepnetsecurity.com>
2017-03-23 18:26:05 +0000

    wizard step one

--- app/controllers/application-wizard.js
+++ app/controllers/application-wizard.js
@@ -4,9 +4,20 @@ export default Ember.Controller.extend({
 
   scenarios: ['---', 'Windows Logon', 'RADIUS', 'Web', '2X'],
   scenario: '---',
+  index: 0,
+  total: 4,
+  percentWidth: Ember.computed('total',function(){
+      return 100 - (100 / this.get('total')) + '%';
+  }),
+  percent: '0%',
   actions: {
     selectScenario(newscenario) { 
     	this.set('scenario', newscenario);
+    },
+    nextStep(){
+        this.incrementProperty('index');
+        let current = this.get('index');
+        this.set('percent', current / (this.get('total') - 1) * 100+'%');
     }
   }
 
