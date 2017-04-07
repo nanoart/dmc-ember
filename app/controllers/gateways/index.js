060d62619f1fdf1740cba7e3c0c976c1b5238cbe
Mingfa Ma <mingfa.ma'deepnetsecurity.com>
2017-04-07 12:44:03 +0100

    table delete does not sync

--- app/controllers/gateways/index.js
+++ app/controllers/gateways/index.js
@@ -25,7 +25,12 @@ export default Ember.Controller.extend({
     }),
     table: Ember.computed('columns', function() {
         return new Table(this.get('columns'), this.model,{enableSync: true});
-    }),      
+    }),
+    isDisabled4Edit: Ember.computed('table.selectedRows',function(){
+        return this.get('table.selectedRows').length !== 1;
+    }),
+
+    isDisabled4Delete: Ember.computed.empty('table.selectedRows'),          
 
     expanded: false,
     actions:{
@@ -33,6 +38,18 @@ export default Ember.Controller.extend({
         {
             this.transitionToRoute('gateways.new');            
         },
+        editGateway()
+        {
+            this.transitionToRoute('gateways.new');            
+        },
+        deleteGateway()
+        {
+          let rows = this.get('table.selectedRows');
+//          alert(rows[0].content.id);
+//we can do the deletion here,as the store is injected. this is simply for learning closure action on component
+//          this.get('deleteSP')(rows[0].content.id); //it can have promise
+          this.get('table').removeRows(rows);            
+        },                
         toggleSourceCodeFull() {
             this.toggleProperty('expanded');
         }        
--- app/routes/gateways.js
+++ app/routes/gateways.js
@@ -3,7 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   model() {
-    return this.store.findAll('gateway');
+//    return this.store.findAll('gateway');
+    //
+    return this.store.findAll('gateway').then(gateways => gateways.toArray()) ;
   }
 
 });
