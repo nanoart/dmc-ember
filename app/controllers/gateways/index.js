2ee374c4f8567151de394835323f6bc93ef95979
mike ma <mike ma>
2017-04-07 08:22:27 +0100

    add table in gateways

--- app/controllers/gateways/index.js
+++ app/controllers/gateways/index.js
@@ -1,6 +1,32 @@
 import Ember from 'ember';
+import Table from 'ember-light-table';
 
 export default Ember.Controller.extend({
+
+    columns: Ember.computed(function() {
+        return [{
+            label: 'Name',
+            valuePath: 'name',
+            width: '30%',
+            sortable: false
+        }, {
+            label: 'Domains',
+            valuePath: 'domains',
+            width: '30%'
+         }, {
+            label: 'Type',
+            valuePath: 'type',
+            width: '30%'
+        }, {        
+            label: 'Enabled',
+            valuePath: 'enabled',
+            width: '30%'
+        }];
+    }),
+    table: Ember.computed('columns', function() {
+        return new Table(this.get('columns'), this.model,{enableSync: true});
+    }),      
+
     expanded: false,
     actions:{
         addGateway()
